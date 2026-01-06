757dcad1693f0a7f90c33af011edbf22f73a1ff3
Tudor73 <tudorminciunescu@gmail.com>
2026-01-06 14:00:02 +0200

    refactor

--- app/commands/streams.go
+++ app/commands/streams.go
@@ -61,60 +61,84 @@ func parseMapValues(args []string) map[string]any {
 	return mapValues
 }
 
+type StreamId struct {
+	Ms  uint64
+	Seq int
+}
+
+func (s StreamId) Compare(other StreamId) int {
+	if s.Ms > other.Ms {
+		return 1
+	}
+	if s.Ms < other.Ms {
+		return -1
+	}
+	if s.Seq > other.Seq {
+		return 1
+	}
+	if s.Seq < other.Seq {
+		return -1
+	}
+	return 0
+}
+
 func validateId(id string, lastEntry *StreamValue) (string, error) {
 
-	miliseconds, sequenceNumber, err := parseId(id)
+	miliseconds, sequenceNumber, autoSeq, err := parseId(id)
 	if err != nil {
 		return "", err
 	}
-	prevMiliseconds, prevNumber := 0, 0
+	var prev StreamId
 	if lastEntry != nil {
-		prevMiliseconds, prevNumber, _ = parseId(lastEntry.Id)
+		// Ideally lastEntry already has a StreamID struct to avoid re-parsing
+		pMs, pSeq, _, _ := parseId(lastEntry.Id)
+		prev = StreamId{Ms: uint64(pMs), Seq: pSeq}
 	}
-	if sequenceNumber == -1 {
-		if miliseconds == prevMiliseconds {
-			sequenceNumber = prevNumber + 1
+
+	if autoSeq {
+		if miliseconds == prev.Ms {
+			sequenceNumber = prev.Seq + 1
 		} else {
 			sequenceNumber = 0
 		}
 	}
-	if miliseconds == 0 && sequenceNumber == 0 {
+
+	current := StreamId{Ms: miliseconds, Seq: sequenceNumber}
+
+	if current.Ms == 0 && current.Seq == 0 {
 		return "", fmt.Errorf("ERR The ID specified in XADD must be greater than 0-0")
 	}
-	if miliseconds < prevMiliseconds {
+	if lastEntry != nil && current.Compare(prev) <= 0 {
 		return "", fmt.Errorf("ERR The ID specified in XADD is equal or smaller than the target stream top item")
 	}
-	if miliseconds == prevMiliseconds && sequenceNumber == prevNumber {
-		return "", fmt.Errorf("ERR The ID specified in XADD is equal or smaller than the target stream top item")
-	}
+
 	return fmt.Sprintf("%d-%d", miliseconds, sequenceNumber), nil
 }
 
-func parseId(id string) (int, int, error) {
+func parseId(id string) (uint64, int, bool, error) {
 	values := strings.Split(id, "-")
 	if len(values) > 2 {
-		return 0, 0, fmt.Errorf("ERR the id specified is not in the right format")
+		return 0, 0, false, fmt.Errorf("ERR the id specified is not in the right format")
 	}
 	if len(values) == 1 {
 		if values[0] != "*" {
-			return 0, 0, fmt.Errorf("ERR the id specified is not in the right format")
+			return 0, 0, false, fmt.Errorf("ERR the id specified is not in the right format")
 		}
 		values[0] = strconv.FormatInt(time.Now().UnixMilli(), 10)
 		values = append(values, "*")
 	}
 
-	if values[1] == "*" {
-		values[1] = "-1"
-	}
-
 	miliseconds, err := strconv.Atoi(values[0])
 	if err != nil {
-		return 0, 0, fmt.Errorf("ERR invalid format")
+		return 0, 0, false, fmt.Errorf("ERR invalid format")
+	}
+	if values[1] == "*" {
+		return uint64(miliseconds), 0, true, nil
 	}
 	sequenceNumber, err := strconv.Atoi(values[1])
 	if err != nil {
-		return 0, 0, fmt.Errorf("ERR invalid format")
+		return 0, 0, false, fmt.Errorf("ERR invalid format")
 	}
 
-	return miliseconds, sequenceNumber, nil
+	return uint64(miliseconds), sequenceNumber, false, nil
 }
